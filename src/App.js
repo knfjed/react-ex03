19378fbe514fead27c6e2a3d6ab8a3f1c31094aa
knfjed <knfjed@gmail.com>
2020-06-22 09:46:26 +0900

    create form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./App.css";
+import Form from "./compornents/Form/Form";
 
 function App() {
   return (
-    <>
-      <div>
+    <div>
+      <div className="logo">
         <h1>Greens</h1>
       </div>
 
@@ -16,17 +18,27 @@ function App() {
         </ul>
       </nav>
 
+      <div className="explanation">
+        <h1>Contact Us</h1>
+        <p>
+          If you have any questions, please fill out the form here. Finally,
+          click the send button. A representative will contact you within 24
+          hours.
+        </p>
+      </div>
+
       <img src={"consulting_image.svg"} alt="イメージ画像" />
 
       <address>
-        <div>
+        <div className="tel">
           <img
             src="https://img.icons8.com/metro/26/000000/phone.png"
             alt="電話のアイコン"
           />
           <p>0120-334-3547</p>
         </div>
-        <div>
+
+        <div className="mail">
           <img
             src="https://img.icons8.com/material-rounded/24/000000/important-mail.png"
             alt="メールのアイコン"
@@ -34,7 +46,9 @@ function App() {
           <p>info@greens.com</p>
         </div>
       </address>
-    </>
+
+      <Form />
+    </div>
   );
 }
 
--- src/compornents/Form/Form.jsx
+++ src/compornents/Form/Form.jsx
@@ -1,3 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form() {}
+export default function Form({ name, email, message }) {
+  const [_name, setName] = React.useState(name);
+  const [_email, setEmail] = React.useState(email);
+  const [_message, setMessage] = React.useState(message);
+
+  return (
+    <>
+      <form>
+        <label>Name</label>
+        <br />
+        <input
+          type="text"
+          value={_name}
+          // onChange={(event) => setName(event.currentTarget.value)}
+        ></input>
+      </form>
+
+      <form>
+        <label>Email</label>
+        <br />
+        <input
+          type="text"
+          value={_email}
+          // onChange={(event) => setEmail(event.currentTarget.value)}
+        ></input>
+      </form>
+
+      <form>
+        <label>Message</label>
+        <br />
+        <input
+          type="text"
+          value={_message}
+          // onChange={(event) => setMessage(event.currentTarget.value)}
+        ></input>
+      </form>
+
+      <button
+        onClick={() =>
+          console.log("data", {
+            name: setName,
+            email: setEmail,
+            message: setMessage,
+          })
+        }
+      >
+        SUBMIT
+      </button>
+    </>
+  );
+}
